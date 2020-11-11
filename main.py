78ea7c45146185e40aded8020b5e37aaea05fe3a
MIachaI <mprawda@wp.pl>
2020-11-11 14:29:17 +0100

    add match analyzing functions

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import re
 initialUrl = 'https://pl.dotabuff.com/players/41771279/matches?enhance=overview&page=57'
 urlPrefix = 'https://pl.dotabuff.com'
 matchesList = []
+matchesDetails = {}
 
 def setPage(url):
     r = requests.get(url,headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"})
@@ -24,14 +25,43 @@ def getNextPage(soup):
 def dotabuffParser(soup):
     getMatches(soup)
     nextPage = getNextPage(soup)
+    print(nextPage)
     if (nextPage != None):
-        dotabuffParser(setPage(urlPrefix + nextPage))
+        #dotabuffParser(setPage(urlPrefix + nextPage))
         print('This is the last page')
+
+def getMatchDetails(soup):
+    matchDetail = {}
+    direTeam = []
+    radiantTeam = []
+    for aElement in soup.find_all(class_=re.compile('player-dire link-type-player')):
+        #matchesList.append(aElement['class'])
+        direTeam.append(aElement.contents[0])
+        #matchDetail[aElement.contents[0]] = "dire"
+
+    for aElement in soup.find_all(class_=re.compile('player-radiant link-type-player')):
+        #matchesList.append(aElement['class'])
+        radiantTeam.append(aElement.contents[0])
+        #matchDetail[aElement.contents[0]] = "radiant"
+    matchDetail['dire'] = direTeam
+    matchDetail['radiant'] = radiantTeam
+    return matchDetail
+
+def runMatchesLoop():
+    #for match in matchesList:
+    for x in range(5):
+
+        soup = setPage(urlPrefix+matchesList[x])
+        #matchID = match.split('/')[2]
+        matchID = matchesList[x].split('/')[2]
+        print(matchID)
+        matchesDetails[matchID] = getMatchDetails(soup)
 
 def main():
     laZuppa = setPage(initialUrl)
     dotabuffParser(laZuppa)
-    print(len(matchesList))
+    runMatchesLoop()
+    print(matchesDetails)
 
 
 main()
